f746094fcf9be9006203e8f75a7c36ee083e8b69
MTviruez <mpviruez@gmail.com>
2022-06-12 13:09:46 -0400

    first commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -17,7 +17,7 @@ const nuevaMultiplicacion = 10 * null === 40 ;
 
 // Resuelve el siguiente problema matemático:
 const nuevoModulo = 21 % 5 === null;
-
+"Hola"
 
 // En los próximos 22 problemas, deberás completar la función.
 // Todo tu código irá dentro de las llaves de la función.
